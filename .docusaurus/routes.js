7a80370a6b85c4af4b684a9d3b8cd4729606427b
gotgrassct <gotgrassct@gmail.com>
2024-10-31 23:09:39 -0400

    new

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,41 +3,6 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/TeamSomber.github.io/__docusaurus/debug',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug', '659'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/config',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/config', '37d'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/content',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/content', 'd1a'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/globalData',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/globalData', 'c1d'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/metadata',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/metadata', 'd36'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/registry',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/registry', '66f'),
-    exact: true
-  },
-  {
-    path: '/TeamSomber.github.io/__docusaurus/debug/routes',
-    component: ComponentCreator('/TeamSomber.github.io/__docusaurus/debug/routes', '4f5'),
-    exact: true
-  },
-  {
     path: '/TeamSomber.github.io/',
     component: ComponentCreator('/TeamSomber.github.io/', '4c4'),
     routes: [
